df754153e91c666730af9fecc9aa5d704fbd2c81
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-03-19 08:49:20 +0530

    all set

--- public/js/script.js
+++ public/js/script.js
@@ -2,24 +2,38 @@ let button=document.getElementById('play_with_computer_button')
 button.addEventListener('click',()=>{
     window.location.href="/computer"
 })
-let headings = ["Let's Joy", "Unlimited playing", "Play With Computer", "Play with Friends"];
+let headings = ["Let's Rock & Joy", "Unlimited playing", "Play With Computer", "Play with Friends"];
 let heading = document.getElementById('heading_changer');
 
+let repeat=true;
 async function display() {
+    heading.style.color="white"
+    heading.style.fontSize="2rem"
     for (let ele = 0; ele < headings.length; ele++) {
-        let text = headings[ele].split("");
+        let text = headings[ele]
         console.log(text)
-        for (let i = 0; i <=text.length; i++) {
-            await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 se
-           
+        console.log(text.length)
+        for (let i = 0; i <text.length; i++) {
+            await new Promise(resolve => setTimeout(resolve, 500)); 
+           console.log(text.slice(0,i))
+           heading.textContent=text.slice(0,i)
     
         }
         let arr=headings[ele]
         for (let i = 0; i < arr.length; i++) {
-            await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 se
-            console.log(arr.slice(0,arr.length-i))
+            await new Promise(resolve => setTimeout(resolve, 300)); 
+           heading.textContent=arr.slice(0,arr.length-i)
         }
+        heading.textContent=" "
     }
+    repeat=true
 }
-
-display();
+setInterval(()=>{
+ if(repeat){
+    repeat=false
+  display()
+ }
+ else{
+    null
+ }
+},500)
